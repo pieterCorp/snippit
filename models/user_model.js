7ea991b5cfdaefacda3335e947d3f5b1da2d18dc
PieterCorp <pieter@email.com>
2021-02-07 11:56:19 +0100

    firstCommit

---
+++ models/user_model.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    SubscribedDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,45 @@
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app =  express();
+
+app.use(express.static('welcomePage'));
+
+//connect to MongoDB
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log("Connected to database"));
+
+
+//algemene middleware
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+//USE USER MIDDLEWARE
+
+const userRoute = require('./routes/users');
+app.use('/users', userRoute);
+
+
+// HOST WELCOMEPAGE
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname})
+});
+
+// SERVER LISTEN
+app.listen(process.env.PORT ||3000, () =>{
+    console.log(`server listening at port 3000`)
+});
+
